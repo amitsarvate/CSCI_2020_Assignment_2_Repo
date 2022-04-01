d62af2ce4684a2611ff61e45bc02936b8f2cf188
Amanda Showler <71836770+amandashowler@users.noreply.github.com>
2022-04-01 12:43:32 -0400

    Update HelloApplication.java

--- src/main/java/com/example/assignment2/HelloApplication.java
+++ src/main/java/com/example/assignment2/HelloApplication.java
@@ -18,42 +18,44 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+       
         CSVReader reader = new CSVReader();
         ArrayList<ArrayList<String>> data;
 
         // Get the CSV data
         data = reader.ReadCSV();
-
+        
+        // Create the bar chart
         CategoryAxis xAxis = new CategoryAxis();
         NumberAxis yAxis = new NumberAxis();
-
         yAxis.setLabel("Number of Incidents");
         xAxis.setLabel("Incidents");
-
         BarChart barChart = new BarChart<>(xAxis,yAxis);
-
         barChart.setTitle("Fatal Incidents Between 85 to 99 vs Fatal Incidents Between 00 to 14 Amongst Airlines");
         barChart.setPrefWidth(1000);
         barChart.setPrefHeight(800);
-
+        
+        // Add 85-99 data to series
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("Fatal Incidents Between 85 to 99");
 
         for (int i = 0; i < data.get(0).size(); i++) {
             series1.getData().add(new XYChart.Data(data.get(0).get(i), Integer.parseInt(data.get(3).get(i))));
         }
-
+        
+        // Add 00-14 data to series
         XYChart.Series series2 = new XYChart.Series();
         series2.setName("Fatal Incidents Between 00 to 14");
 
         for (int i = 0; i < data.get(0).size(); i++) {
             series2.getData().add(new XYChart.Data(data.get(0).get(i), Integer.parseInt(data.get(6).get(i))));
         }
-
+        
+        // Add all data to bar chart
         barChart.getData().addAll(series1, series2);
 
+        // Add the chart to the scene of the application
         StackPane rootPane = new StackPane();
-
         Scene scene = new Scene(rootPane, 1000, 800);
         Pane pane = new Pane(barChart);
         rootPane.getChildren().addAll(pane);
@@ -68,6 +70,7 @@ public class HelloApplication extends Application {
 
         // Run function to create the XML files
         airline.assignment2();
+        // NOTE: XML Files don't update until the application is closed
 
         // Launch application for Part 3
         launch();
